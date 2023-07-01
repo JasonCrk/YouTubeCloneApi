d0faeb419f062491fc73edcc98d34a971c2203e8
JasonCrk <emerzonkolki@gmail.com>
2023-07-01 16:35:43 -0500

    fix: remove user urls and DB_PORT to int

--- apps/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from apps.user.views import ListUsers
-
-urlpatterns = [
-   path('', ListUsers.as_view())
-]
--- youtube_clone/urls.py
+++ youtube_clone/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
-    path('api/users/', include('apps.user.urls')),
 
     path('admin/', admin.site.urls)
 ]
