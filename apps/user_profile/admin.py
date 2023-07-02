fb023f8ab001ddf3c1ca6bdba13a498b4e708f08
JasonCrk <emerzonkolki@gmail.com>
2023-07-02 16:56:20 -0500

    feat: profile model to admin

--- apps/user_profile/admin.py
+++ apps/user_profile/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'picture', 'theme', 'languages']
+    list_filter = ['theme', 'languages']
+    fieldsets = [
+        (None, { 'fields': ['user', 'picture'] }),
+        ('Settings', { 'fields': ['theme', 'languages'] }),
+    ]
+
+    search_fields = ['theme', 'languages']
+    ordering = ['user']
+    filter_horizontal = []
+
+admin.site.register(Profile, ProfileAdmin)
