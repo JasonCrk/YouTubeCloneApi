112cb54eda6e817fc639b1da3166099578896e31
JasonCrk <emerzonkolki@gmail.com>
2023-07-02 17:22:00 -0500

    fix: relationship of Profile and User

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
-from apps.user.models import UserAccount
+from django.conf import settings
 
 from .themes import THEMES
 from .languages import LANGUAGES
 
+User = settings.AUTH_USER_MODEL
+
 class Profile(models.Model):
-    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     picture = models.URLField(verbose_name='Picture or Avatar', null=True, blank=True)
     theme = models.CharField(choices=THEMES, default='light')
     languages = models.CharField(choices=LANGUAGES, default='EN')
