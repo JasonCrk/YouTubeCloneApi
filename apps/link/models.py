0545cd4fdbbbae4f76b7a015c7b408c72dc253ef
JasonCrk <emerzonkolki@gmail.com>
2023-07-04 15:04:45 -0500

    feat: create Link model and register in admin

---
+++ apps/link/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from apps.channel.models import Channel
+
+class Link(models.Model):
+    position = models.PositiveSmallIntegerField()
+    title = models.CharField(max_length=15)
+    url = models.URLField()
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self) -> str:
+        return self.title
