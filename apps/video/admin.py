d8369740066783c1db6c51bc35aeb1a37b3b03cf
JasonCrk <emerzonkolki@gmail.com>
2023-07-05 17:15:22 -0500

    feat: add views property to the video model

--- apps/video/admin.py
+++ apps/video/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from apps.video.models import Video
+from apps.video.models import Video, VideoView
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'channel', 'video_url', 'publication_date']
     search_fields = ['title', 'description']
     ordering = ['title']
 
+class VideoViewAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'video', 'count', 'last_view_date']
+    ordering = ['last_view_date']
+
 admin.site.register(Video, VideoAdmin)
+admin.site.register(VideoView, VideoViewAdmin)
--- apps/video/models.py
+++ apps/video/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from apps.channel.models import Channel
+from apps.user_profile.models import Profile
 
 class Video(models.Model):
     title = models.CharField(max_length=45)
@@ -8,9 +9,19 @@ class Video(models.Model):
     description = models.TextField(null=True, blank=True)
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
     publication_date = models.DateTimeField(auto_now_add=True, blank=True)
+    views = models.ManyToManyField(Profile, through='VideoView')
 
     class Meta:
         ordering = ['title']
 
     def __str__(self) -> str:
         return self.title
+
+class VideoView(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    count = models.PositiveBigIntegerField(default=1)
+    last_view_date = models.DateTimeField(auto_now=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.video.title
