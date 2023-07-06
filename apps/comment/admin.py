0bc862ad469eb501fc0cb3e5dd24eba309aa653e
JasonCrk <emerzonkolki@gmail.com>
2023-07-06 09:57:44 -0500

    feat: likedComment model and likes property
    Create LikedComment model and add likes property to the Comment model

--- apps/comment/admin.py
+++ apps/comment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.comment.models import Comment
+from apps.comment.models import Comment, LikedComment
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['profile', 'video', 'comment', 'publication_date', 'was_edited']
@@ -8,4 +8,10 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ['content']
     ordering = ['content']
 
+class LikedCommentAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'comment', 'liked']
+    list_filter = ['liked']
+    ordering = ['profile']
+
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(LikedComment, LikedCommentAdmin)
--- apps/comment/models.py
+++ apps/comment/models.py
@@ -10,9 +10,18 @@ class Comment(models.Model):
     content = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True, blank=True)
     was_edited = models.BooleanField(default=False)
+    likes = models.ManyToManyField(Profile, through='LikedComment', related_name='comment_likes')
 
     class Meta:
         ordering = ['publication_date']
+
+    def __str__(self) -> str:
+        return self.profile.user.username
+
+class LikedComment(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    liked = models.BooleanField(default=True, blank=True)
 
     def __str__(self) -> str:
         return self.profile.user.username
