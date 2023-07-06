e1ca44cfba0af45b2acd61cd9b8d4e619337672c
JasonCrk <emerzonkolki@gmail.com>
2023-07-06 08:10:00 -0500

    feat: channelSubscription model and subscription property
    Create ChannelSubscripton model and add subscription property to the Channel model

--- apps/channel/admin.py
+++ apps/channel/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from apps.channel.models import Channel
+from apps.channel.models import Channel, ChannelSubscription
 
 class ChannelAdmin(admin.ModelAdmin):
     list_display = ['user_profile', 'handle', 'joined']
     search_fields = ['handle']
     ordering = ['user_profile']
 
+class ChannelSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'channel', 'subscription_date']
+    ordering = ['profile']
+
 admin.site.register(Channel, ChannelAdmin)
+admin.site.register(ChannelSubscription, ChannelSubscriptionAdmin)
