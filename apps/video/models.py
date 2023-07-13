5fdd6076e3aecc2d521a5fdbc09fb3bae331d25a
JasonCrk <emerzonkolki@gmail.com>
2023-07-13 09:07:27 -0500

    fix: add thumbnail property from Video model

--- apps/video/models.py
+++ apps/video/models.py
@@ -7,6 +7,7 @@ from apps.user.models import UserAccount
 class Video(models.Model):
     title = models.CharField(max_length=45)
     video_url = models.URLField()
+    thumbnail = models.URLField()
     description = models.TextField(null=True, blank=True)
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
     publication_date = models.DateTimeField(auto_now_add=True, blank=True)
