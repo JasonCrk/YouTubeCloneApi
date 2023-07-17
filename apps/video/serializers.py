e48354fe29f0ff290ac608e8d08a202596a909cb
JasonCrk <emerzonkolki@gmail.com>
2023-07-17 14:31:58 -0500

    feat: like and dislike video

--- apps/video/serializers.py
+++ apps/video/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.video.models import Video
+from apps.video.models import Video, LikedVideo
 
 
 class ValidationVideoSerializer(serializers.ModelSerializer):
@@ -14,4 +14,15 @@ class ValidationVideoSerializer(serializers.ModelSerializer):
             'description',
             'video',
             'thumbnail'
+        )
+
+
+class VideoLikeValidatorSerializer(serializers.ModelSerializer):
+    video_id = serializers.IntegerField()
+
+    class Meta:
+        model = LikedVideo
+        fields = (
+            'liked',
+            'video_id'
         )
