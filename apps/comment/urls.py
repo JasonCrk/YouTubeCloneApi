2ab568e4dcbf36d7324b1bcb8ec16553a517d9c6
JasonCrk <emerzonkolki@gmail.com>
2023-07-23 16:06:35 -0500

    feat: create comment for comment

--- apps/comment/urls.py
+++ apps/comment/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from apps.comment import views
 
 urlpatterns = [
-    path('create/<int:video_id>', views.CreateVideoCommentView.as_view(), name='create_video_comment')
+    path(
+        'create/<int:video_id>/',
+        views.CreateVideoCommentView.as_view(),
+        name='create_video_comment'
+    ),
+    path(
+        '<int:comment_id>/create/',
+        views.CreateCommentForCommentView.as_view(),
+        name='create_comment_for_comment'
+    )
 ]
