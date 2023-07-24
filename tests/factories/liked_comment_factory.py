2964905e46ca6c7bbd5157d8add2f1af409fa2d8
JasonCrk <emerzonkolki@gmail.com>
2023-07-24 17:53:30 -0500

    test(comment): add tests like comment and factory

---
+++ tests/factories/liked_comment_factory.py
@@ -0,0 +1,21 @@
+import factory
+
+from tests.factories.channel_factory import ChannelFactory
+from tests.factories.comment_factory import CommentFactory
+
+from apps.comment.models import LikedComment
+
+
+class BaseLikedCommentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = LikedComment
+
+    channel = factory.SubFactory(ChannelFactory)
+    comment = factory.SubFactory(CommentFactory)
+
+
+class LikeCommentFactory(BaseLikedCommentFactory):
+    class Meta:
+        model = LikedComment
+
+    liked = True
