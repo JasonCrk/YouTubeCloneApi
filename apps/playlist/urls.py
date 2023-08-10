40941172216b939777b509417ac73e5b2b807c24
JasonCrk <emerzonkolki@gmail.com>
2023-08-09 21:44:30 -0500

    feat: save video to playlist endpoint

--- apps/playlist/urls.py
+++ apps/playlist/urls.py
@@ -4,5 +4,6 @@ from apps.playlist import views
 
 urlpatterns = [
     path('create/', views.CreatePlaylistView.as_view(), name='create_playlist'),
+    path('<int:playlist_id>/save-video/', views.SaveVideoToPlaylistView.as_view(), name='save_video_to_playlist'),
     path('<int:playlist_id>/edit/', views.EditPlaylistView.as_view(), name='edit_playlist')
 ]
