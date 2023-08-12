ef6fb5e3a58706e52bdb4bd7e282e3460b33aa7f
JasonCrk <emerzonkolki@gmail.com>
2023-08-11 23:15:32 -0500

    feat(api): delete playlist endpoint

--- apps/playlist/urls.py
+++ apps/playlist/urls.py
@@ -3,8 +3,29 @@ from django.urls import path
 from apps.playlist import views
 
 urlpatterns = [
-    path('create/', views.CreatePlaylistView.as_view(), name='create_playlist'),
-    path('<int:playlist_id>/save-video/', views.SaveVideoToPlaylistView.as_view(), name='save_video_to_playlist'),
-    path('<int:playlist_id>/edit/', views.EditPlaylistView.as_view(), name='edit_playlist'),
-    path('video/<int:playlist_video_id>/remove', views.RemoveVideoFromPlaylistView.as_view(), name='remove_video_from_playlist')
+    path(
+        'create/',
+        views.CreatePlaylistView.as_view(),
+        name='create_playlist'
+    ),
+    path(
+        '<int:playlist_id>/save-video/',
+        views.SaveVideoToPlaylistView.as_view(),
+        name='save_video_to_playlist'
+    ),
+    path(
+        '<int:playlist_id>/edit/',
+        views.EditPlaylistView.as_view(),
+        name='edit_playlist'
+    ),
+    path(
+        '<int:playlist_id>/delete/',
+        views.DeletePlaylistView.as_view(),
+        name='delete_playlist'
+    ),
+    path(
+        'video/<int:playlist_video_id>/remove',
+        views.RemoveVideoFromPlaylistView.as_view(),
+        name='remove_video_from_playlist'
+    )
 ]
