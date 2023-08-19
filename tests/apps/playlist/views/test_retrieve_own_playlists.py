12ab69f8384e3bec3f749cd1f616f6c404ad8ae6
JasonCrk <emerzonkolki@gmail.com>
2023-08-19 16:17:12 -0500

    test(playlist): add tests to retrieve own playlists

---
+++ tests/apps/playlist/views/test_retrieve_own_playlists.py
@@ -0,0 +1,41 @@
+from django.urls import reverse
+
+from rest_framework import status
+
+from tests.setups import APITestCaseWithAuth
+
+from tests.factories.playlist import PlaylistFactory
+
+from apps.playlist.models import Playlist
+
+from apps.playlist.serializers import PlaylistListSerializer
+
+
+class TestRetrieveOwnPlaylists(APITestCaseWithAuth):
+    def setUp(self):
+        super().setUp()
+
+        self.own_playlist: Playlist = PlaylistFactory.create(channel=self.user.current_channel)
+
+        self.url = reverse('own_playlists')
+
+    def test_return_own_playlists(self):
+        PlaylistFactory.create_batch(2)
+
+        response = self.client.get(self.url)
+
+        retrieve_playlists = [dict(playlist)['id'] for playlist in response.data.get('data')]
+
+        self.assertEqual(len(retrieve_playlists), 1)
+        self.assertIn(self.own_playlist.pk, retrieve_playlists)
+
+    def test_return_serialized_own_playlists(self):
+        serialized_own_playlist = PlaylistListSerializer(self.own_playlist)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertDictEqual(
+            serialized_own_playlist.data,
+            response.data.get('data')[0]
+        )
